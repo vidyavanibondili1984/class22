123a516608f27d3a25784036bd19403dec1c999b
vidyavanibondili1984 <72088524+vidyavanibondili1984@users.noreply.github.com>
2020-12-24 21:50:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+var engine,world;
+var object;
+function setup() {
+  createCanvas(400,400);
+
+  engine=Engine.create();
+  world=engine.world;
+  var options={isStatic:true}
+  object=Bodies.rectangle(200,200,50,50,options);
+  World.add(world,object);
+  console.log(object);
+  console.log(object.position.x);
+ 
+}
+
+function draw() {
+  background(0); 
+  Engine.update(engine);
+  rectMode(CENTER); 
+  rect(object.position.x,object.position.y,50,50)
+  drawSprites();
+}
